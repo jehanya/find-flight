fcbb0430f43856653a1b52d573e8a94cbb6a3989
jehanya <switchf0ot02@gmail.com>
2021-07-31 22:59:09 +0800

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+var swiper = new Swiper('.home--slide', {
+    slidesPerView: 2,
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+});
+
+var swiper = new Swiper('.recommendation--slide2', {
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+$('.input-group').datepicker({
+    format: "yyyy M dd D"
+});
